01d3dbad01bea36f712e66f753748a20b14ce0c2
tanvirtbf <tanver018765@gmail.com>
2024-03-31 17:53:37 +0600

    understand alert and confirm method deeply

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// document.getElementById('hello').style.fontSize="100px"
-console.log(firstName)
-var firstName = "Tanvir"
-let lastName = "Ahmed"
-let age = 15;
-const yearOfBirth = 1998
-console.log(firstName)
-// const userIntro = "Hi , My name is " + firstName + lastName ;
+// alert("Hello World")
+const bool = confirm("Hiii")
+console.log(bool)
