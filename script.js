5fd736162c4d93a7dcbd115ac146e60389d553b3
tanvirtbf <tanver018765@gmail.com>
2024-03-31 10:35:23 +0600

    understand memory creation phase and code execution phase and serve e picture in image folder and show it browser

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementById('hello').style.fontSize="100px"
+
+var firstName = "Tanvir"
+var lastName = "Ahmed"
+let age = 15;
+const userIntro = "Hi , My name is " + firstName + lastName ;
