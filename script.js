c2aa0fd2b79329faae58740e67e460e0f05eac87
tanvirtbf <tanver018765@gmail.com>
2024-04-01 11:15:59 +0600

    start String method with Argument

--- script.js
+++ script.js
@@ -5,21 +5,34 @@
 // trimStart()
 // trimEnd()
 
-const names = 'Tanvir'
-console.log(names[2]) //result : n
-console.log('helloWorld'[6])
-const message = "Hello World"
-console.log(message.length)
-const upperMessage = message.toUpperCase();
-console.log(upperMessage)
-const lowerMessage = message.toLowerCase();
-console.log(lowerMessage)
-const trimMethod = '         hello world        ';
-console.log(trimMethod.trim())
-const trimStart = trimMethod.trimStart();
-console.log(trimStart)
-const trimEnd = trimMethod.trimEnd();
-console.log(trimEnd)
-const chaining = '   hello world   '.trim().toUpperCase(); //chaining
-console.log(chaining)
+// const names = 'Tanvir'
+// console.log(names[2]) //result : n
+// console.log('helloWorld'[6])
+// const message = "Hello World"
+// console.log(message.length)
+// const upperMessage = message.toUpperCase();
+// console.log(upperMessage)
+// const lowerMessage = message.toLowerCase();
+// console.log(lowerMessage)
+// const trimMethod = '         hello world        ';
+// console.log(trimMethod.trim())
+// const trimStart = trimMethod.trimStart();
+// console.log(trimStart)
+// const trimEnd = trimMethod.trimEnd();
+// console.log(trimEnd)
+// const chaining = '   hello world   '.trim().toUpperCase(); //chaining
+// console.log(chaining)
+
+// Methods (With Argument)
+// includes()
+// indexOf()
+// replace()
+// replaceAll()
+// concat()
+// padStart()
+// padEnd()
+// charAt()
+// charCodeAt()
+// split()
+
 
