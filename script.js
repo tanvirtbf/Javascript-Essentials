de3ed915f60ad2908a2025f400db3ab0d43a4bd8
tanvirtbf <tanver018765@gmail.com>
2024-04-01 09:57:42 +0600

    understand string length and uppercase lowercase with devtools and code editor

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const names = 'Tanvir'
-console.log(names[2]) //result : n
-console.log('helloWorld'[6])
+// const names = 'Tanvir'
+// console.log(names[2]) //result : n
+// console.log('helloWorld'[6])
+const message = "Hello World"
+console.log(message.length)
+const upperMessage = message.toUpperCase();
+console.log(upperMessage)
+const lowerMessage = message.toLowerCase();
+console.log(lowerMessage)
