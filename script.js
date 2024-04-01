ccb466f15ed3ca60e8ac7008e8e49f4f70af4821
tanvirtbf <tanver018765@gmail.com>
2024-04-01 10:08:07 +0600

    trim method with trimEnd and trimStart, and string chaining method

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // const names = 'Tanvir'
 // console.log(names[2]) //result : n
 // console.log('helloWorld'[6])
-const message = "Hello World"
-console.log(message.length)
-const upperMessage = message.toUpperCase();
-console.log(upperMessage)
-const lowerMessage = message.toLowerCase();
-console.log(lowerMessage)
+// const message = "Hello World"
+// console.log(message.length)
+// const upperMessage = message.toUpperCase();
+// console.log(upperMessage)
+// const lowerMessage = message.toLowerCase();
+// console.log(lowerMessage)
+const trimMethod = '         hello world        ';
+console.log(trimMethod.trim())
+const trimStart = trimMethod.trimStart();
+console.log(trimStart)
+const trimEnd = trimMethod.trimEnd();
+console.log(trimEnd)
+const chaining = '   hello world   '.trim().toUpperCase(); //chaining
+console.log(chaining)
