aab3a65bf5e6d73193bd211ba16efcdfeab47571
tanvirtbf <tanver018765@gmail.com>
2024-04-01 10:09:50 +0600

    String Method without Argument

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// const names = 'Tanvir'
-// console.log(names[2]) //result : n
-// console.log('helloWorld'[6])
-// const message = "Hello World"
-// console.log(message.length)
-// const upperMessage = message.toUpperCase();
-// console.log(upperMessage)
-// const lowerMessage = message.toLowerCase();
-// console.log(lowerMessage)
+// Methods (Without Argument)
+// toUpperCase()
+// toLowerCase()
+// trim()
+// trimStart()
+// trimEnd()
+
+const names = 'Tanvir'
+console.log(names[2]) //result : n
+console.log('helloWorld'[6])
+const message = "Hello World"
+console.log(message.length)
+const upperMessage = message.toUpperCase();
+console.log(upperMessage)
+const lowerMessage = message.toLowerCase();
+console.log(lowerMessage)
 const trimMethod = '         hello world        ';
 console.log(trimMethod.trim())
 const trimStart = trimMethod.trimStart();
@@ -15,3 +22,4 @@ const trimEnd = trimMethod.trimEnd();
 console.log(trimEnd)
 const chaining = '   hello world   '.trim().toUpperCase(); //chaining
 console.log(chaining)
+
