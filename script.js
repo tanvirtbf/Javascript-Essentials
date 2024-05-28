bda5c3dbf1a0a21ef18dd3da6cb59fd2314ad478
tanvirtbf <tanver018765@gmail.com>
2024-05-28 14:08:49 +0600

    seeing null , undefined variable address

--- script.js
+++ script.js
@@ -102,7 +102,10 @@
 
 // How to see variable address in dev tools
 const firstName = 'Tanvir'
+const firstRule = 'Tanvir'
 const userAge = 15
 const isGraduate = false
 const hasJob = false
 const isStudent = true
+const isNull = null
+const isUndefined = undefined
