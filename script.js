0114968a5c0808019748859921cbb86e4a98cceb
tanvirtbf <tanver018765@gmail.com>
2024-06-14 15:40:05 +0600

    star Deep copy shallow copy

--- script.js
+++ script.js
@@ -156,13 +156,15 @@
 // console.log(typeof(arr))
 // console.log(typeof(obj))
 
-const fruits = ['Mango','Banana','Jackfruits','Grapes','Dates']
+// const fruits = ['Mango','Banana','Jackfruits','Grapes','Dates']
 
-console.log(fruits.push('Chal','Dal'))
-console.log(fruits.pop())
-console.log(fruits.shift())
-console.log(fruits.unshift())
-console.log(fruits.concat(['hello','gelo']))
-console.log(fruits.indexOf('Banana'))
-console.log(fruits.includes('Mango'))
-console.log(fruits.reverse())
+// console.log(fruits.push('Chal','Dal'))
+// console.log(fruits.pop())
+// console.log(fruits.shift())
+// console.log(fruits.unshift())
+// console.log(fruits.concat(['hello','gelo']))
+// console.log(fruits.indexOf('Banana'))
+// console.log(fruits.includes('Mango'))
+// console.log(fruits.reverse())
+
+
