23848a8faaed231c5c554dbf3c937dfffa92ac03
tanvirtbf <tanver018765@gmail.com>
2024-07-23 21:21:49 +0600

    update

--- script.js
+++ script.js
@@ -721,11 +721,12 @@
 // li3.previousElementSibling.previousElementSibling //li1
 // li3.previousElementSibling.previousElementSibling.previousElementSibling // null
 
-// Node
+// Node Node Node ChildNode
 
 //Text Node change
 // let nodes = document.body.childNodes
 // nodes = document.body.childNodes[2]
+// document.body.childNodes[1].childNodes[0].nodeValue = 'Hello World'
 // nodes = document.body.childNodes[2].nodeValue
 
 // document.body.childNodes[2].nodeValue = 'Hii Tanvir!!'
@@ -830,35 +831,35 @@
 
 // simple project using document.createElement
 
-let container = document.body.querySelector(".container");
-let x = 1;
-//let myHtml ;
+// let container = document.body.querySelector(".container");
+// let x = 1;
+// //let myHtml ;
 
-for (let i = 1; i <= 100; i++) {
-  // myHtml = myHtml + `
-  //     <div class="divCon">
-  //       <img src='./image/me${x}.JPG' alt="">
-  //       <p class="para">${i}</p>
-  //     </div>
-  //   `;
+// for (let i = 1; i <= 100; i++) {
+//   // myHtml = myHtml + `
+//   //     <div class="divCon">
+//   //       <img src='./image/me${x}.JPG' alt="">
+//   //       <p class="para">${i}</p>
+//   //     </div>
+//   //   `;
 
-  let div = document.createElement('div')
-  let image = document.createElement("img");
-  let paragraph = document.createElement('h4')
-  container.appendChild(div)
-  div.appendChild(image);
-  div.appendChild(paragraph)
-  div.classList.add('divCon')
-  paragraph.innerHTML = `<i>Image ${i} </i>`
-  image.src = `./image/me${x}.JPG`;
-  image.alt = `image${i}`;
+//   let div = document.createElement('div')
+//   let image = document.createElement("img");
+//   let paragraph = document.createElement('h4')
+//   container.appendChild(div)
+//   div.appendChild(image);
+//   div.appendChild(paragraph)
+//   div.classList.add('divCon')
+//   paragraph.innerHTML = `<i>Image ${i} </i>`
+//   image.src = `./image/me${x}.JPG`;
+//   image.alt = `image${i}`;
 
-  x++;
-  if (i % 3 == 0) {
-    x = 1;
-  }
-}
-//container.innerHTML = myHtml
+//   x++;
+//   if (i % 3 == 0) {
+//     x = 1;
+//   }
+// }
+// //container.innerHTML = myHtml
 
 // Remove Element -- 2 Way 
 //1. OLD Way
