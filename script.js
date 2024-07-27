5c56cf6666072b20e68268f6e246f1bb7456b3f0
tanvirtbf <tanver018765@gmail.com>
2024-07-27 10:26:16 +0600

    some example and note

--- script.js
+++ script.js
@@ -125,12 +125,26 @@
 // ekhane same variable address na hoyeo output true ashce karon primitive data type kokhono memory address diye compare kore na . primitive data type value diye compare kore . value same hoile true hobe jodio memory address change thakuk
 
 //Objects => Non-premitive Data Type
+const firstName = 'Tanvir' // @1757053
+const lastName = 'Tan'+'vir' // @2457343
+console.log(firstName === lastName) // Ans : true
 const user1 = {
   firstName: 'Tanvir',
   'last Name': 'Ahmed',
   age: 25,
   'hasJob': true,
-}
+} // @620689 
+const user2 = {
+  firstName: 'Tanvir',
+  'last Name': 'Ahmed',
+  age: 25,
+  'hasJob': true,
+} // @620689
+console.log(user1===user2) // Ans : false
+
+// Note : user1 and user2 er value same diye create kora holeo tader variable address same na and user1 === user2 is false . karon object non-primitive . tai ekhane value compare na hoye memory address compare hoise . 
+
+
 // const user2 = {
 //   'firs tName': 'Sadia',
 //   'last Name': 'Ahmed',
