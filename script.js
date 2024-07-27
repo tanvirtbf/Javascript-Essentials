fb09902658257972a3fe3c0850c167e3d726d78d
tanvirtbf <tanver018765@gmail.com>
2024-07-27 10:17:38 +0600

    revision Objects => Non-premitive Data Type

--- script.js
+++ script.js
@@ -115,22 +115,22 @@
 // const isUndefined = undefined // @67 fixed for undefined value
 
 // Important Note : 
-const firstName = 'Tanvir' // @1757053 address
-const name = 'Tanvir' // @1757053 address 
-console.log(firstName === name ) // Ans : true
-// But....
-const fname = 'Tanvir' // @1757053
-const lname = 'Tan' + 'vi' + 'r' // @2676783
-console.log(fname === lname) // Ans : true
+// const firstName = 'Tanvir' // @1757053 address
+// const name = 'Tanvir' // @1757053 address 
+// console.log(firstName === name ) // Ans : true
+// // But....
+// const fname = 'Tanvir' // @1757053
+// const lname = 'Tan' + 'vi' + 'r' // @2676783
+// console.log(fname === lname) // Ans : true
 // ekhane same variable address na hoyeo output true ashce karon primitive data type kokhono memory address diye compare kore na . primitive data type value diye compare kore . value same hoile true hobe jodio memory address change thakuk
 
 //Objects => Non-premitive Data Type
-// const user1 = {
-//   firstName: 'Tanvir',
-//   'last Name': 'Ahmed',
-//   age: 25,
-//   'hasJob': true,
-// }
+const user1 = {
+  firstName: 'Tanvir',
+  'last Name': 'Ahmed',
+  age: 25,
+  'hasJob': true,
+}
 // const user2 = {
 //   'firs tName': 'Sadia',
 //   'last Name': 'Ahmed',
