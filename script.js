69b8cc1f481c9174f12d916f283cac826e7ae538
tanvirtbf <tanver018765@gmail.com>
2024-08-01 16:13:40 +0600

    concat method

--- script.js
+++ script.js
@@ -583,11 +583,22 @@
 // console.log(arr) // [1, 2, 3, 4, 5]
 
 // Splice Method 
-const arr = [1,2,3,4,5]
-console.log(arr) // [1, 2, 3, 4, 5]
-const spliceArr = arr.splice(2,4) 
-console.log(spliceArr) // [3, 4, 5]
-console.log(arr) // [1, 2] 
+// const arr = [1,2,3,4,5]
+// console.log(arr) // [1, 2, 3, 4, 5]
+// const spliceArr = arr.splice(2,4) 
+// console.log(spliceArr) // [3, 4, 5]
+// console.log(arr) // [1, 2] 
+
+// concat method 
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+// arr1.push(arr2)
+// console.log(arr1) // [1,2,3,[4,5,6]]
+// push method change the original array but concat is create a new Array
+const newArr = arr1.concat(arr2)
+console.log(newArr) // [1, 2, 3, 4, 5, 6]
+console.log(arr1) // [1, 2, 3]
+
 
 
 
