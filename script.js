62dae609085166bb7299797518f7eb724173b7a7
tanvirtbf <tanver018765@gmail.com>
2024-08-01 16:15:22 +0600

    spread operator

--- script.js
+++ script.js
@@ -590,14 +590,20 @@
 // console.log(arr) // [1, 2] 
 
 // concat method 
+// const arr1 = [1,2,3]
+// const arr2 = [4,5,6]
+// // arr1.push(arr2)
+// // console.log(arr1) // [1,2,3,[4,5,6]]
+// // push method change the original array but concat is create a new Array
+// const newArr = arr1.concat(arr2)
+// console.log(newArr) // [1, 2, 3, 4, 5, 6]
+// console.log(arr1) // [1, 2, 3]
+
+// spread operator
 const arr1 = [1,2,3]
 const arr2 = [4,5,6]
-// arr1.push(arr2)
-// console.log(arr1) // [1,2,3,[4,5,6]]
-// push method change the original array but concat is create a new Array
-const newArr = arr1.concat(arr2)
-console.log(newArr) // [1, 2, 3, 4, 5, 6]
-console.log(arr1) // [1, 2, 3]
+const newArr = [...arr1, ...arr2]
+console.log(newArr)
 
 
 
