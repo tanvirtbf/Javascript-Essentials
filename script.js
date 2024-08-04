5220a6926ff183bcfebed235e86b0cf2c4c8c19e
tanvirtbf <tanver018765@gmail.com>
2024-08-04 16:14:10 +0600

    dog api

--- script.js
+++ script.js
@@ -1949,4 +1949,10 @@
 
 // Advance Javascript (Async)
 // JSON 
-
+// let data;
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => {
+//         console.log(json)
+//         data = json;
+//       })
