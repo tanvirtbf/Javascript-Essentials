05e4b4f8697cde128c88064b54c522d482944dde
tanvirtbf <tanver018765@gmail.com>
2024-08-07 11:16:36 +0600

    .then() and .catch() understand deeply

--- script.js
+++ script.js
@@ -2261,8 +2261,26 @@
 
 // .then and .catch
 
+const resolveBtn = document.querySelector('#resolveBtn')
+const rejectBtn = document.querySelector('#rejectBtn')
+const p = new Promise((resolve,reject)=>{
+  resolveBtn.addEventListener('click',()=>{
+    resolve({fname:'Tanvir',lname:'Ahmed',age: 25,})
+  })
+  rejectBtn.addEventListener('click',()=>{
+    reject('Promise Rejected!')
+  })
+})
 
+// console.log(p) korle prototype name e ekta object ase . oi object er vitore 3 ta special object ase . 1. catch() 2. finally() 3. then() 
 
+// rules of then() -> jokhonii promise resolve hoye jabe tokhonii then() method call hoye jabe 
+// rules of then() -> then() method tar vitore ekta callback function accept kore thake
+p.then(()=>{
+  console.log('promise resolved') // jokhon p promise resolved hobe tokhon ai console log print hobe
+}).catch(()=>{
+  console.log('promise rejected') // jokhon p promise reject hobe tokhon ai console log print hobe
+})
 
 
 
