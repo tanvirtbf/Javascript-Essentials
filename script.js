9c052d09619c9ec43b3d2f90426a1090f58cfdbd
tanvirtbf <tanver018765@gmail.com>
2024-08-10 11:09:50 +0600

    recap callback problem

--- script.js
+++ script.js
@@ -2482,6 +2482,16 @@
 // upore onk important ekta api createOrder api er control ami nijer kache na rekhe addToCart api er moddhe diye rakhsi . ekhetre ami blindly trust kortesi addToCart api ke je se tar kaj sesh kore createOrder api ke call korbe . 
 // addToCart api er personal bugs thakte pare . ai addToCart api ke aro onk developer change kore dite pare . tar jonno effect porbe createOrder api er upor .
 
+// Recap two problem of callback
+// Two issues while using callbacks
+
+// 1 - Callback hell
+// When a function is passed as an argument to another function, it becomes a callback function. This process continues and there are many callbacks inside another's Callback function.
+// This grows the code horizontally instead of vertically. That mechanism is known as callback hell. 
+
+// 2 - Inversion of control
+// The callback function is passed to another callback, this way we lose the control of our code. We don't know what is happening behind the scene and the program becomes very difficult to maintain. 
+// That process is called inversion of control. 
 
 // Promises
 // Promise is a special type of object
