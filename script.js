7c6340536413f007e3c8b6ebc4dd7883cda89d0d
tanvirtbf <tanver018765@gmail.com>
2024-08-11 12:23:02 +0600

    set common function for all new create user

--- script.js
+++ script.js
@@ -3245,3 +3245,25 @@
 // console.log(createUser.prototype.constructor === user2.__proto__.constructor) // true
 // console.log(user1.__proto__.constructor === user2.__proto__.constructor) // true
 // // tar mane createUser er vitor jei constructor function ase segula user1 , user2 and jotogula user create hobe sob gular moddhei same memory address er function thakbe
+
+// set common function for all user
+function createUser(fname,lname,age){
+  this.firstName = fname
+  this.lastName = lname
+  this.age = age
+}
+
+createUser.prototype.getBirthYear = function(){
+  return new Date().getFullYear() - this.age
+}
+
+const user1 = new createUser('Tanvir','Ahmed',25)
+const user2 = new createUser('Sadia','Ahmed',15)
+const user3 = new createUser('Maymuna','Khatun',18)
+console.log(user1.getBirthYear()) // 1999
+console.log(user2.getBirthYear()) // 2009
+console.log(user3.getBirthYear()) // 2006
+
+
+
+
