44cc53b93096de61364a20522f29beae17059c70
tanvirtbf <tanver018765@gmail.com>
2024-08-20 12:23:11 +0600

    project complete without save data in localstorage

--- focus on today/script.js
+++ focus on today/script.js
@@ -11,6 +11,10 @@ const goalInput = document.querySelectorAll(".goal-input")
 
 const goalContainer = document.querySelectorAll('.goal-container')
 
+let calculate = []
+calculate[0] = false
+calculate[1] = false
+calculate[2] = false
 
 const inputObj = {
   first: '',
@@ -32,13 +36,25 @@ customCheckbox.forEach((item,index)=>{
   item.addEventListener('click',()=>{
     if(inputObj.first && inputObj.second && inputObj.third){
       errorLabel.style.display = 'none'
+      calculate[index] = !calculate[index]
       goalContainer.forEach((value,i)=>{
         if(index===i){
           value.classList.toggle('completed')
         }
       })
+      let newCal = calculate.filter((item)=> item)
+      if(newCal.length===1){
+        progressValue.style.width = '33.4%'
+      }else if(newCal.length===2){
+        progressValue.style.width = '66.7%'
+      }else if(newCal.length===3){
+        progressValue.style.width = '100%'
+      }else if(newCal.length===0){
+        progressValue.style.width = '0px'
+      }
     }else{
       errorLabel.style.display = 'block'
     }
   })
 })
+
