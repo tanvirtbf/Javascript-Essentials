4838deb352cc6137eff4757088bb66aff283f816
tanvirtbf <tanver018765@gmail.com>
2024-08-20 11:21:29 +0600

    project html css complete and some javascript

---
+++ focus on today/script.js
@@ -0,0 +1,34 @@
+const firstInput = document.querySelector("#first")
+const secondInput = document.querySelector("#second")
+const thirdInput = document.querySelector("#third")
+
+const progressValue = document.querySelector(".progress-value")
+const errorLabel = document.querySelector(".error-label")
+
+const customCheckbox = document.querySelectorAll(".custom-checkbox")
+
+const inputObj = {
+  first: '',
+  second: '',
+  third: '',
+}
+
+firstInput.addEventListener("input",(e)=>{
+  inputObj.first = e.target.value
+})
+secondInput.addEventListener("input",(e)=>{
+  inputObj.second = e.target.value
+})
+thirdInput.addEventListener("input",(e)=>{
+  inputObj.third = e.target.value
+})
+
+customCheckbox.forEach((item)=>{
+  item.addEventListener('click',()=>{
+    if(inputObj.first && inputObj.second && inputObj.third){
+      console.log('true')
+    }else{
+      console.log('false')
+    }
+  })
+})
