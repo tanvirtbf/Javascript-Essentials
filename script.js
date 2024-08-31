bd610d2551dbc14048a4dea363f88ec897c6c983
tanvirtbf <tanver018765@gmail.com>
2024-08-31 18:05:07 +0600

    update

--- script.js
+++ script.js
@@ -4949,26 +4949,31 @@
 
 
 // Private 
-// class createUser{
-//   #age
-//   constructor(fname,lname,age){
-//     this.firstName = fname;
-//     this.lastName = lname;
-//     this.#age = age;
-//     console.log('Hello World')
-//   }
-//   getFullName(){
-//     return this.firstName + " " + this.lastName
-//   }
-//   getBirthYear(){
-//     return new Date().getFullYear() - this.#age
-//   }
-// }
-// const user1 = new createUser('Tanvir','Ahmed',25)
-// const user2 = new createUser('Sadia','Hatun',16)
+class createUser{
+  #age // jekono private variable niye kaj korar age eta ke age evabe declare korte hoy # diye
+  constructor(fname,lname,age){
+    this.firstName = fname;
+    this.lastName = lname;
+    this.#age = age;
+    console.log('Hello World')
+  }
+  h1 = 'hello world!' // (ai variable ta user1, user2, user3... sob variable e aisha porbe)
+  #h2 = 'Hello'
+  getFullName(){
+    return this.firstName + " " + this.lastName
+  }
+  getBirthYear(){
+    return new Date().getFullYear() - this.#age
+  }
+  #privateFun(){
+    console.log('Hello Private Function')
+  }
+}
+const user1 = new createUser('Tanvir','Ahmed',25)
+const user2 = new createUser('Sadia','Hatun',16)
 
-// console.dir(user2.getFullName()) // Sadia Hatun
-// console.log(user2.getBirthYear()) // 2008
+console.dir(user2.getFullName()) // Sadia Hatun
+console.log(user2.getBirthYear()) // 2008
 // console.log(user2.#age) // error (karon eta private property)
 
 
