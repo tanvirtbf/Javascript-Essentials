76f10424916ab256a9834f1c6d28c7ba24cdedc2
tanvirtbf <tanver018765@gmail.com>
2024-12-23 20:52:19 +0600

    Implicit type conversion code example

--- Frontend Master Javascript/Second Class/app.js
+++ Frontend Master Javascript/Second Class/app.js
@@ -1,1 +1,15 @@
-console.log('Hello!')
+// Type Conversion Code 
+
+// Implicit Type Conversion
+// console.log('5' - 2) // 3
+
+// console.log('Hello' - 2) // NaN (karon ekhane type conversion kora somvob na)
+// console.log('Hello' + 2) // Hello2 (ekhane type conversion somvob tai kore nise string e)
+
+// boolean to number
+// console.log(true + 1) // 2
+// console.log(true + 10) // 11
+// console.log(false + 1) // 1
+// console.log(false - 3) // -3
+
+// Explicit Type Conversion 
