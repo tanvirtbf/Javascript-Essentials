ed21987cc60eb4c75b4373cd93ff05f598a89f88
tanvirtbf <tanver018765@gmail.com>
2025-01-04 10:09:56 +0600

    Hoisting complete

---
+++ Frontend Master Javascript/Third Class/Hoisting/app.js
@@ -0,0 +1,15 @@
+// console.log(jk)
+// var jk = 100
+
+// function jks(){
+//   console.log(jk)
+// }
+// jks()
+
+
+// var hello = 'Hello World!'
+
+// var hello = function(){
+//   console.log('Hello Hi Bye!')
+// }
+// hello()
