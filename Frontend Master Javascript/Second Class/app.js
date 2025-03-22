e355ac3db643660c01a3f80502f61b8391122651
tanvirtbf <tanver018765@gmail.com>
2025-03-22 23:10:13 +0600

    update

--- Frontend Master Javascript/Second Class/app.js
+++ Frontend Master Javascript/Second Class/app.js
@@ -1,10 +1,3 @@
-// Type Conversion Code 
-
-// Implicit Type Conversion
-// console.log('5' - 2) // 3
-
-// console.log('Hello' - 2) // NaN (karon ekhane type conversion kora somvob na)
-// console.log('Hello' + 2) // Hello2 (ekhane type conversion somvob tai kore nise string e)
 
 // boolean to number
 // console.log(true + 1) // 2
