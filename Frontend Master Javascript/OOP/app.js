4d821428af3e5676cff00b5fa627371d670b0484
tanvirtbf <tanver018765@gmail.com>
2025-03-30 17:19:09 +0600

    Inheritance Achieve

--- Frontend Master Javascript/OOP/app.js
+++ Frontend Master Javascript/OOP/app.js
@@ -23,17 +23,38 @@
 
 
 // Constructor 
-class Person {
-  myName = 'Tanvir Ahmed'
+// class Person {
+//   myName = 'Tanvir Ahmed'
 
-  constructor (name){
-    this.myName = name
+//   constructor (name){
+//     this.myName = name
+//     console.log(this.myName)
+//   }
+//   showName (){
+//     return this.myName
+//   }
+// }
+
+// const person1 = new Person("Tanvir")
+// console.log(person1)
+
+
+
+// Inheritance
+class Parent {
+  myName = 'Tanvir'
+  myAge = 23
+  showName (){
     console.log(this.myName)
   }
-  showName (){
-    return this.myName
-  }
 }
+const person1 = new Parent()
 
-const person1 = new Person("Tanvir")
-console.log(person1)
+class Child extends Parent {
+
+}
+const childOfPerson1 = new Child()
+console.log(childOfPerson1)
+
+
+
