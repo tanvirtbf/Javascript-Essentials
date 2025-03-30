1876b8535e7a6f753aa1bf1344bfb658b792aab1
tanvirtbf <tanver018765@gmail.com>
2025-03-30 13:17:51 +0600

    same goes with object

--- Frontend Master Javascript/OOP/app.js
+++ Frontend Master Javascript/OOP/app.js
@@ -1,11 +1,21 @@
-class Person {
-  myName = 'Tanvir Ahmed'
+// class Person {
+//   myName = 'Tanvir Ahmed'
 
+//   showName (){
+//     return this.myName
+//   }
+// }
+
+// const person1 = new Person()
+// console.log(person1.showName())
+
+
+// Same this goes in object
+
+const obj = {
+  myName : 'Tanvir',
   showName (){
     return this.myName
   }
 }
-
-const person1 = new Person()
-console.log(person1.showName())
-
+console.log(obj.showName())
