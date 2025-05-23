4c266cf771a20ef8ddfc5dbbd42d92af24b845ab
tanvirtbf <tanver018765@gmail.com>
2025-05-23 15:50:56 +0600

    cllbck strt

---
+++ Frontend Master Javascript/Async & Event Loop/index.js
@@ -0,0 +1,17 @@
+// callback code example : 
+const num = 500;
+function getData(callback){
+    let i=0;
+    while (i>1000){
+        i++
+    }
+    const num2 = i
+    callback(num2)
+}
+
+getData(addIt)
+
+function addIt(num2){
+    const result = num2 + num;
+    console.log(result)
+}
