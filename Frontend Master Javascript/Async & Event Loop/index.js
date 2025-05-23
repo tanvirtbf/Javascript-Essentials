81f6a1eef1c6006367853826d3aa55800ef114db
tanvirtbf <tanver018765@gmail.com>
2025-05-23 16:56:33 +0600

    promises code example

--- Frontend Master Javascript/Async & Event Loop/index.js
+++ Frontend Master Javascript/Async & Event Loop/index.js
@@ -1,17 +1,36 @@
 // callback code example : 
-const num = 500;
-function getData(callback){
-    let i=0;
-    while (i<1000){
-        i++
-    }
-    const num2 = i
-    callback(num2)
-}
+// const num = 500;
+// function getData(callback){
+//     let i=0;
+//     while (i<1000){
+//         i++
+//     }
+//     const num2 = i
+//     callback(num2)
+// }
 
-getData(addIt)
+// getData(addIt)
 
-function addIt(num2){
-    const result = num2 + num;
-    console.log(result)
-}
+// function addIt(num2){
+//     const result = num2 + num;
+//     console.log(result)
+// }
+
+
+// Promises code Example : 
+const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Data Recieved Successfully")
+    }, 3000)
+})
+
+let loading = true
+
+promise.then((data) => {
+    console.log(data, 'data')
+}).catch((err) => {
+    console.log(err, 'error')
+}).finally(() => {
+    loading=false
+    console.log("finally")
+})
