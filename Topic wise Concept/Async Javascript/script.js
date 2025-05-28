ccd1207bdc32e827864246053a534236a1496806
tanvirtbf <tanver018765@gmail.com>
2025-05-28 11:31:38 +0600

    callback example

--- Topic wise Concept/Async Javascript/script.js
+++ Topic wise Concept/Async Javascript/script.js
@@ -1,17 +1,28 @@
-console.log(1)
-setTimeout(() =>{
-    console.log(2)
-}, 2000)
-setTimeout(() =>{
-    console.log(3)
-},1000)
-setTimeout(() =>{
-    console.log(4)
-}, 3000)
-setTimeout(() =>{
-     console.log(5)   
-})
-setTimeout(() =>{
-    console.log(6)
-}, 1500)
-console.log(7)
+// console.log(1)
+// setTimeout(() =>{
+//     console.log(2)
+// }, 2000)
+// setTimeout(() =>{
+//     console.log(3)
+// },1000)
+// setTimeout(() =>{
+//     console.log(4)
+// }, 3000)
+// setTimeout(() =>{
+//      console.log(5)   
+// })
+// setTimeout(() =>{
+//     console.log(6)
+// }, 1500)
+// console.log(7)
+// // result : 1 7 5 3 6 2 4 
+
+// Callback example 
+function sum(){
+    console.log('Hello World!');
+}
+
+function message(callback){
+    callback()
+}
+message(sum)
