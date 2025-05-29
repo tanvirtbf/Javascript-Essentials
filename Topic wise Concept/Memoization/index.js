75fe1ec38bf209a7ba3417c9ebeb35f17daf4dbf
tanvirtbf <tanver018765@gmail.com>
2025-05-29 12:06:15 +0600

    Memoized closure way declared

--- Topic wise Concept/Memoization/index.js
+++ Topic wise Concept/Memoization/index.js
@@ -72,3 +72,40 @@ console.log(sum());
 console.log(sum());
 
 // ekhn ami function ta ke memoized kore dilam . but eta optimized way na . jodio eta kaj korbe . 
+
+
+
+
+
+// Closure way : 
+function memoizedSum(){
+    let result = null
+    return function(){
+        if(result) return result
+        let i=0;
+        while(i<10000000){
+            i++;
+        }
+        result = 1+1
+        return 1+1
+    }
+}
+
+const sum = memoizedSum()
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
