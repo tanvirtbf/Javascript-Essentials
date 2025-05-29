5f0843ea1cc5d532af242832ba37897ffc8dafc4
tanvirtbf <tanver018765@gmail.com>
2025-05-29 12:44:59 +0600

    Dynamic way of memoization with closure

--- Topic wise Concept/Memoization/index.js
+++ Topic wise Concept/Memoization/index.js
@@ -78,34 +78,62 @@
 
 
 // Closure way : 
+// function memoizedSum(){
+//     let result = null
+//     return function(){
+//         if(result) return result
+//         let i=0;
+//         while(i<10000000000){
+//             i++;
+//         }
+//         result = 1+1
+//         return 1+1
+//     }
+// }
+
+// const sum = memoizedSum()
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+
+
+
+// Dynamic way 
 function memoizedSum(){
-    let result = null
-    return function(){
-        if(result) return result
-        let i=0;
-        while(i<10000000000){
-            i++;
+    let result = {}
+    return function(n1, n2){
+        if(result[`${n1},${n2}`]){
+            console.log("Cached");
+            return result[`${n1},${n2}`]
         }
-        result = 1+1
-        return 1+1
+
+        const sumResult = n1+n2;
+        result[`${n1},${n2}`] = sumResult;
+        return sumResult
     }
 }
 
 const sum = memoizedSum()
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
-console.log(sum());
+console.log(sum(2,3));
+console.log(sum(3,1));
+console.log(sum(3,1));
+console.log(sum(5,1));
+console.log(sum(4,2));
+console.log(sum(4,2));
+console.log(sum(4,2));
+console.log(sum(9,2));
+console.log(sum(1,1));
