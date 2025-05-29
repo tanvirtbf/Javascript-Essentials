7df4f837203d9c79a68486e1fc2ce1bbae7e9a87
tanvirtbf <tanver018765@gmail.com>
2025-05-29 09:55:12 +0600

    Memoization note added

---
+++ Topic wise Concept/Memoization/index.js
@@ -0,0 +1,74 @@
+// function sum(){
+//     let i=0;
+//     while(i<1000000){
+//         i++;
+//     }
+//     return 1+1;
+// }
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+// console.log(sum());
+
+// ekhane sum function barbar call hocche but same output dicche karon tar argument same , sob gula call e same jinish return hocche . tahole eta ke kivabe amra memoize korte pari ? 
+
+// Answer : 
+
+var result = null
+
+function sum(){
+    if(result){
+        return result
+    }
+    let i=0;
+    while(i<1000000000){
+        i++;
+    }
+    result = 1+1
+    return 1+1
+}
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+console.log(sum());
+
+// ekhn ami function ta ke memoized kore dilam . but eta optimized way na . jodio eta kaj korbe . 
