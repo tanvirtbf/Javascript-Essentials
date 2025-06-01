07bd913782b9f39382ec3f9675779aa127146b60
tanvirtbf <tanver018765@gmail.com>
2025-06-01 14:16:03 +0600

    infinite loop example

--- Topic wise Concept/Event Loop/index.js
+++ Topic wise Concept/Event Loop/index.js
@@ -1,13 +1,26 @@
-function demo(){
-    const box = document.getElementByClassName("box-r")
+// function demo(){
+//     const box = document.getElementByClassName("box-r")
 
-    box.style.backgroundColor = "blue"
-    box.style.backgroundColor = "red"
-    box.style.backgroundColor = "green"
-    box.style.backgroundColor = "yellow"
-    box.style.backgroundColor = "lime"
-    box.style.backgroundColor = "purple"
-    box.style.backgroundColor = "orange"
-    box.style.backgroundColor = "maroon"
+//     box.style.backgroundColor = "blue"
+//     box.style.backgroundColor = "red"
+//     box.style.backgroundColor = "green"
+//     box.style.backgroundColor = "yellow"
+//     box.style.backgroundColor = "lime"
+//     box.style.backgroundColor = "purple"
+//     box.style.backgroundColor = "orange"
+//     box.style.backgroundColor = "maroon"
 
+// }
+
+
+// Infinite Loop
+function move(){
+    setTimeout(() =>{
+        let i=0
+        while(i<1000000000){
+            i++
+        }
+        console.log(1);
+        move()
+    })
 }
