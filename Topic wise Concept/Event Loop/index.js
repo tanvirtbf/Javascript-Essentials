b02b9b78290b6cfd50497b318e640be7070f28ae
tanvirtbf <tanver018765@gmail.com>
2025-06-01 09:26:04 +0600

    start Rendering pipelines

---
+++ Topic wise Concept/Event Loop/index.js
@@ -0,0 +1,13 @@
+function demo(){
+    const box = document.getElementByClassName("box-r")
+
+    box.style.backgroundColor = "blue"
+    box.style.backgroundColor = "red"
+    box.style.backgroundColor = "green"
+    box.style.backgroundColor = "yellow"
+    box.style.backgroundColor = "lime"
+    box.style.backgroundColor = "purple"
+    box.style.backgroundColor = "orange"
+    box.style.backgroundColor = "maroon"
+
+}
