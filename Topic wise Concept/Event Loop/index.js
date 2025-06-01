bb3899f3de834b2791d003837068a3f4dcba80d9
tanvirtbf <tanver018765@gmail.com>
2025-06-01 17:02:04 +0600

    start Event Loop

--- Topic wise Concept/Event Loop/index.js
+++ Topic wise Concept/Event Loop/index.js
@@ -26,6 +26,33 @@
 // }
 
 // Infinite Loop with Micro (promises)
-function move(){
-    Promise.resolve().then(move)
-}
+// function move(){
+//     Promise.resolve().then(move)
+// }
+
+
+// Event Loop Example Code : 
+console.log(1);
+
+setTimeout(() =>{
+    console.log(2);
+})
+
+Promise.resolve().then(() =>{
+    console.log(3);
+})
+
+requestAnimationFrame(() =>{
+    console.log(4);
+})
+
+console.log(5);
+
+// output : 
+// 1
+// 5
+// 3
+// 2
+// 4
+
+// kintu 2 er age 4 ashar kotha . kintu na . event loop jokhon RAF e ashbe tokhon dekhbe akhn ki ui update er somoy ? answer is no . karon tokhon 16ms hoy nai . karon browser kokhonoii sathe sathe ui update kore na . se changes gula batch kore rakhe and frame create er somoy ui update kore . kintu macro theke thaka setTimeout tokhon executable thakay seta age execute hoye gese 
