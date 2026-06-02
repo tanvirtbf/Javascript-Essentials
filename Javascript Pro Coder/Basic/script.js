e37890d9d5fe26779227f7216ee742d14b8d4cf6
Tanvir Ahmed <tanver018765@gmail.com>
2026-06-02 11:14:51 +0600

    update firstName variable to an array and log its type

--- Javascript Pro Coder/Basic/script.js
+++ Javascript Pro Coder/Basic/script.js
@@ -1,6 +1,10 @@
-var firstName = "Tanvir";
+console.log(firstName, 'type : ' , typeof(firstName))
+
+var firstName = [1,2,3]
 var lastName = "Ahmed";
 let age = 15;
 const yearOfBirth = 1999;
 
 let userIntro = "Hi, my name is " + firstName + " " + lastName;
+
+
